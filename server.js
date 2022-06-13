fb1858038d2fe7fda384b4c451d5f6a2faa3bb4f
Glitch (hello-express) <none>
2022-06-13 20:22:14 +0000

    🦃🌯 Checkpoint ./lib/definitions.js:45781378/12 ./server.js:45781378/2

--- server.js
+++ server.js
@@ -6286,7 +6286,7 @@ var maintainloop = (() => {
   return () => {
     // Do stuff
     makenpcs();
-    makefood();
+    //makefood();
     // Regen health and update the grid
     entities.forEach((instance) => {
       if (instance.shield.max) {

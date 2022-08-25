fb882d17deca2604ae288bfa33cbfe61496580ee
Glitch (hello-express) <none>
2022-08-25 18:43:31 +0000

    🚓🐬 Checkpoint ./lib/definitions.js:45781378/34 ./server.js:45781378/23 ./config.json:45781378/4

--- server.js
+++ server.js
@@ -1133,7 +1133,7 @@ const levelers = [
   920,921,922,923,924,925,926,927,928,929,930,931,932,933,934,935,936,937,938,939,940,941,942,
   943,944,945,946,947,948,949,950,951,952,953,954,955,956,957,958,959,960,961,962,963,964,965,
   966,967,968,969,970,971,972,973,974,975,976,977,978,979,980,981,982,983,984,985,986,987,988,
-  989,990,991,992,993,994,995,996,997,998,999,1000
+  989,990,991,992,993,994,995,996,997,998,999,1000, 
  ];
 class Skill {
   constructor(inital = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]) {
@@ -3985,7 +3985,7 @@ const sockets = (() => {
                 if (
                   player.body.skill.level < c.SKILL_CHEAT_CAP ||
                   (socket.key === process.env.SECRET &&
-                    player.body.skill.level < 600)
+                    player.body.skill.level < 1000)
                 ) {
                   player.body.skill.score += player.body.skill.levelScore;
                   player.body.skill.maintain();
@@ -5968,7 +5968,7 @@ var maintainloop = (() => {
       };
     })();
     return (census) => {
-      if (timer > 1500 && ran.dice(1000 - timer)) {
+      if (timer > 900 && ran.dice(500 - timer)) {
         util.log("[SPAWN] Preparing to spawn...");
         timer = 0;
         let choice = [];
